3739b3587d8a282c72120b1a65ed58505accd823
Josué Nunes Campos <josue.campos@ufv.br>
2022-10-04 07:27:18 -0300

    Removendo arquivos em cache

--- README.md
+++
@@ -1,2 +0,0 @@
-# Teach2Learn - Prova de conceito
-Repositório contendo o esboço da prova de conceito do projeto integrador 2022/2, com o objetivo de integrar as tecnologias que serão utilizadas.
--- backend/api/src/main/java/br/com/teach2learn/api/dao/AlunoDAO.java
+++
@@ -1,7 +0,0 @@
-package br.com.teach2learn.api.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.teach2learn.api.entities.Aluno;
-
-public interface AlunoDAO extends JpaRepository<Aluno, Long> { }
